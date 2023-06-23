86ee168d0342624bd0dad1dba7749d4a531bfe04
Jun <wjs1157@naver.com>
2023-06-23 10:06:20 +0900

    am commit

--- src/main/reactjs/src/App.js
+++ src/main/reactjs/src/App.js
@@ -6,7 +6,7 @@ function App() {
     <div className="App">
       <img alt='' src={poimg} width={300} border="1" style={{marginTop:'20px'}}/>
       <br/><br/>
-      <h2>성공인가요~~</h2>
+      <h2>배포 성공!!</h2>
     </div>
   );
 }
---
+++ src/main/reactjs/src/member/MemberList.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import '../App.css';
+
+function MemberList(props) {
+    return (
+        <div>
+            <h1>Member List</h1>
+        </div>
+    );
+}
+
+export default MemberList;
